8d14474a8eef3048e63ddd96c8b540d5eab1185e
lomik31 <radiostepan@gmail.com>
2022-01-08 14:07:50 +0300

    Добавлена рандомность в начислении процента банка

--- rec_file.py
+++ rec_file.py
@@ -17,7 +17,7 @@ def time_nachislenie(fileRead):
     return fileRead
 def bank_nachislenie(fileRead):
     for i in fileRead["users"]:
-        add = int(fileRead["users"][i]["bank"] * 0.02931)
+        add = int(fileRead["users"][i]["bank"] * random.uniform(0.002, 0.03))
         fileRead["users"][i]["bank"] += add
         fileRead["users"][i]["earnedKkh"] += add
     return fileRead
